6a6667b6bf626db0ee26097101b519e6efcaa6f5
billtrn <dtran5@conncoll.edu>
2021-02-04 23:45:30 +0700

    feat: refactor

--- preprocess.py
+++ preprocess.py
@@ -1,35 +1,40 @@
-from os.path import join
-
 import os
 import re
 import string
 import pandas as pd
+from os.path import join
 
 PATTERN = r"(train_[0-9]{1,6}\n\"(.|\n)*?\"\n[0|1])"
 
 
-def load_data(filename):
+def preprocess(filename):
     """Load a text database into an excel file with 2 columns, text and label for binary classification"""
     # Load the train dataset
     file = join(os.getcwd(), "data", filename)
+
     with open(file, encoding="utf-8") as infile:
         content = infile.read()
+
     content_list = re.findall(PATTERN, content)
     content_list = [i[0] for i in content_list]
     data = []
+
     for item in content_list:
         row = {}
         label = int(item.split("\n")[-1])
         text = item.split("\n")[1:-1]
         text = " ".join([i for sublist in text for i in sublist.split(" ")]).lower()
         text = text.translate(str.maketrans("", "", string.punctuation))
+
         if len(text) > 2:
             row["text"] = text
             row["label"] = label
             data.append(row)
+
     df = pd.DataFrame(data)
     excel_file = join(os.getcwd(), "data", "corpus", filename.replace("crash", "xlsx"))
     df.to_excel(excel_file, index=None)
 
 
-load_data("train.crash")
+if __name__ == "__main__":
+    preprocess("train.crash")
--- train.py
+++ train.py
@@ -9,30 +9,36 @@ from load_data import load_dataset
 
 
 def save_model(filename, clf):
-    """Train and save the model using training data"""
+    """Save the model as a pickle file"""
     pickle.dump(clf, open(filename, "wb"))
 
 
-train_path = join(dirname(__file__), "data", "corpus", "train.xlsx")
-serialization_dir = join(dirname(__file__), "snapshots")
-print("Load data...")
-X_train, y_train = load_dataset(train_path)
+def train():
+    """Train a model based on training data"""
+    train_path = join(dirname(__file__), "data", "corpus", "train.xlsx")
+    serialization_dir = join(dirname(__file__), "snapshots")
+    print("Load data...")
+    X_train, y_train = load_dataset(train_path)
 
-print("\nTraining model...")
-t0 = time()
-# Go through BOW to find the weight of each word
-transformer = CountVectorizer()
-X_train = transformer.fit_transform(X_train)
+    print("\nTraining model...")
+    t0 = time()
+    # Go through BOW to find the weight of each word
+    transformer = CountVectorizer()
+    X_train = transformer.fit_transform(X_train)
 
-# Modeling using Logistic Regression -> estimator
-model = LogisticRegression(max_iter=1000)
-estimator = model.fit(X_train, y_train)
-t1 = time() - t0
-print("Train time: %0.3fs" % t1)
+    # Modeling using Logistic Regression -> estimator
+    model = LogisticRegression(max_iter=1000)
+    estimator = model.fit(X_train, y_train)
+    t1 = time() - t0
+    print("Train time: %0.3fs" % t1)
 
-print("\nSave model...")
-t0 = time()
-save_model(serialization_dir + "/x_transformer.pkl", transformer)
-save_model(serialization_dir + "/model.pkl", estimator)
-t1 = time() - t0
-print("Save model time: %0.3fs" % t1)
+    print("\nSave model...")
+    t0 = time()
+    save_model(serialization_dir + "/x_transformer.pkl", transformer)
+    save_model(serialization_dir + "/model.pkl", estimator)
+    t1 = time() - t0
+    print("Save model time: %0.3fs" % t1)
+
+
+if __name__ == "__main__":
+    train()
