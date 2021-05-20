71874ee61b53bf1faca07fcb4d9da8e209b36de2
Bill <trantriducs@gmail.com>
2021-05-20 14:19:12 +0700

    feat: add test for exception case

--- tests/engines/test_evaluate.py
+++ tests/engines/test_evaluate.py
@@ -1,6 +1,6 @@
 import os
 
-from main.engines import evaluate_file, evaluate_text
+from main.engines import evaluate_file, evaluate_text, evaluate_translated_text
 
 
 def test_evaluate_text():
@@ -17,3 +17,11 @@ def test_evaluate_file():
     evaluate_file(test_data_path="data/test.crash", output_directory="tests/test_data")
 
     assert "result.csv" in os.listdir("tests/test_data")
+
+
+def test_evaluate_text_with_exception(mocker):
+    mocker.patch(
+        "main.engines.evaluate.translate_to_training_language", side_effect=Exception
+    )
+
+    assert evaluate_translated_text("Tốt quá").sentiment == 0
