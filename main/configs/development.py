9b1361c6cfc74c8308cfe59eeecf2e1f60179e60
Bill <trantriducs@gmail.com>
2021-05-21 21:50:16 +0700

    feat: support docker installation

--- main/configs/development.py
+++ main/configs/development.py
@@ -1,15 +1,15 @@
+import os
+
 from main.configs.base import Config
 
 
 class DevelopmentConfig(Config):
     # Memcached
-    MEMCACHED_SERVER = ("localhost", 11211)
+    MEMCACHED_SERVER = (os.getenv("MEMCACHED_HOST", "localhost"), 11211)
     CACHE_PREFIX = "sentiment"
     CACHE_TIME_OUT = 3600
 
     TRAINING_LANGUAGE = "vi"
-
-    DEBUG = True
 
 
 config = DevelopmentConfig
